f0d15d7cbdb511c9a1f2e8713a8a76ebcd360ed9
dev-MichaelCruz <michaelcruz.dev@gmail.com>
2024-11-16 15:53:58 -0300

    sec: apply Throttler

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { error } from 'console';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [ 
     forwardRef(() => UserModule),
-    forwardRef(() => AuthModule)
+    forwardRef(() => AuthModule),
+    ThrottlerModule.forRoot([{
+      ttl: 60000,
+      limit: 10
+    }])
   ],
   controllers: [ AppController ],
-  providers: [ AppService ],
+  providers: [ 
+    AppService, 
+    { provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    }
+  ],
 })
 export class AppModule {}
